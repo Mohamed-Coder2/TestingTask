e1f13975a7317bcd5bed27ecf0204aced69836db
Mohamed <mohamedemad8862@gmail.com>
2024-05-01 10:17:05 +0300

    Added Invalid login testing and Valid Login testing.

---
+++ src/main/java/org/example/SeleniumTask.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTask {
+    @Test
+    public void testValidLogin() {
+        System.setProperty("webdriver.firefox.driver","E:\\Moo\\Chrome Downloads\\geckodriver-v0.34.0-win-aarch64\\geckodriver.exe");
+
+        // Initialize a Web Driver with your favourite browser (Chrome didn't work for me)
+        WebDriver webDriver = new FirefoxDriver();
+
+        // Navigate to a GitHub
+        webDriver.navigate().to("https://www.github.com/login");
+
+        // Send Credentials
+        webDriver.findElement(By.id("login_field")).sendKeys("japaneseyt8862@gmail.com");
+        webDriver.findElement(By.id("password")).sendKeys("123456@_abcdef");
+
+        // Login
+        webDriver.findElement(By.name("commit")).click();
+
+        String ActualValue = webDriver.findElement(By.className("AppHeader-context-item-label")).getText();
+        String ExpectedValue = "Dashboard";
+
+        Assert.assertEquals(ExpectedValue,ActualValue);
+
+        webDriver.close();
+    }
+    @Test
+    public void testInValidLogin() {
+        System.setProperty("webdriver.firefox.driver","E:\\Moo\\Chrome Downloads\\geckodriver-v0.34.0-win-aarch64\\geckodriver.exe");
+
+        // Initialize a Web Driver with your favourite browser (Chrome didn't work for me)
+        WebDriver webDriver = new FirefoxDriver();
+
+        // Navigate to a GitHub
+        webDriver.navigate().to("https://www.github.com/login");
+
+        // Send Credentials
+        webDriver.findElement(By.id("login_field")).sendKeys("japaneseyt8862@gmail.com");
+        webDriver.findElement(By.id("password")).sendKeys("123456@_abcdeg"); // wrong password
+
+        // Login
+        webDriver.findElement(By.name("commit")).click();
+
+        String ActualValue = webDriver.findElement(By.className("js-flash-alert")).getText();
+        String ExpectedValue = "Incorrect username or password.";
+
+        Assert.assertEquals(ExpectedValue,ActualValue);
+
+        webDriver.close();
+    }
+}
